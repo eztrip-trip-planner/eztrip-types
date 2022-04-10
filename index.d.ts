7c4de1d183d6d3c1ef81c66f4ce4ecaa1229a18b
Nick <nickas951@gmail.com>
2022-04-10 09:47:06 +0200

    Update

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,85 @@
-export declare type Maybe<T> = T | undefined | null;
-export declare type EnumKeyOf<T> = keyof T;
+// Basic
+export * from './src/primitive';
+export * from './src/typed-array';
+export * from './src/basic';
+export * from './src/observable-like';
+
+// Utilities
+export { Nullable } from './src/nullable';
+export { Except } from './src/except';
+export { Mutable } from './src/mutable';
+export { Merge } from './src/merge';
+export { MergeExclusive } from './src/merge-exclusive';
+export { RequireAtLeastOne } from './src/require-at-least-one';
+export { RequireExactlyOne } from './src/require-exactly-one';
+export { RequireAllOrNone } from './src/require-all-or-none';
+export { RemoveIndexSignature } from './src/remove-index-signature';
+export { PartialDeep } from './src/partial-deep';
+export { ReadonlyDeep } from './src/readonly-deep';
+export { LiteralUnion } from './src/literal-union';
+export { Promisable } from './src/promisable';
+export { Opaque } from './src/opaque';
+export { InvariantOf } from './src/invariant-of';
+export { SetOptional } from './src/set-optional';
+export { SetRequired } from './src/set-required';
+export { ValueOf } from './src/value-of';
+export { PromiseValue } from './src/promise-value';
+export { AsyncReturnType } from './src/async-return-type';
+export { ConditionalExcept } from './src/conditional-except';
+export { ConditionalKeys } from './src/conditional-keys';
+export { ConditionalPick } from './src/conditional-pick';
+export { UnionToIntersection } from './src/union-to-intersection';
+export { Stringified } from './src/stringified';
+export { FixedLengthArray } from './src/fixed-length-array';
+export { MultidimensionalArray } from './src/multidimensional-array';
+export { MultidimensionalReadonlyArray } from './src/multidimensional-readonly-array';
+export { IterableElement } from './src/iterable-element';
+export { Entry } from './src/entry';
+export { Entries } from './src/entries';
+export { SetReturnType } from './src/set-return-type';
+export { Asyncify } from './src/asyncify';
+export { Simplify } from './src/simplify';
+export { Jsonify } from './src/jsonify';
+export { Schema } from './src/schema';
+export { LiteralToPrimitive } from './src/literal-to-primitive';
+export {
+    PositiveInfinity,
+    NegativeInfinity,
+    Finite,
+    Integer,
+    Float,
+    NegativeFloat,
+    Negative,
+    NonNegative,
+    NegativeInteger,
+    NonNegativeInteger,
+} from './src/numeric';
+export { StringKeyOf } from './src/string-key-of';
+
+// Template literal types
+export { CamelCase } from './src/camel-case';
+export { CamelCasedProperties } from './src/camel-cased-properties';
+export { CamelCasedPropertiesDeep } from './src/camel-cased-properties-deep';
+export { KebabCase } from './src/kebab-case';
+export { KebabCasedProperties } from './src/kebab-cased-properties';
+export { KebabCasedPropertiesDeep } from './src/kebab-cased-properties-deep';
+export { PascalCase } from './src/pascal-case';
+export { PascalCasedProperties } from './src/pascal-cased-properties';
+export { PascalCasedPropertiesDeep } from './src/pascal-cased-properties-deep';
+export { SnakeCase } from './src/snake-case';
+export { SnakeCasedProperties } from './src/snake-cased-properties';
+export { SnakeCasedPropertiesDeep } from './src/snake-cased-properties-deep';
+export { ScreamingSnakeCase } from './src/screaming-snake-case';
+export { DelimiterCase } from './src/delimiter-case';
+export { DelimiterCasedProperties } from './src/delimiter-cased-properties';
+export { DelimiterCasedPropertiesDeep } from './src/delimiter-cased-properties-deep';
+export { Join } from './src/join';
+export { Split } from './src/split';
+export { Trim } from './src/trim';
+export { Includes } from './src/includes';
+export { Get } from './src/get';
+export { LastArrayElement } from './src/last-array-element';
+
+// Miscellaneous
+export { PackageJson } from './src/package-json';
+export { TsConfigJson } from './src/tsconfig-json';
